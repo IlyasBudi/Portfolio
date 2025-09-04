0509161bf934c8ccb1634e0c60f916e4fbf89bd9
IlyasBudi <wjati145@gmail.com>
2025-09-04 22:35:36 +0700

    edited dracula theme color

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -166,11 +166,11 @@ const DraculaTheme: Theme = {
   colors: {
   background: '#272935',
   foreground: '#bf95fa',
-  primary: '#bf95fa',
+  primary: '#ff7ac6',
   primaryForeground: '#272935',
   secondary: '#44475a',
   secondaryForeground: '#272935',
-  accent: '#ff7ac6',
+  accent: '#bf95fa',
   accentForeground: '#272935',
   muted: '#272935',
   mutedForeground: '#abb0bf',
